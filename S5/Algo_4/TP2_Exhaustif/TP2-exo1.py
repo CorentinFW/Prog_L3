d0c3320d85a3ba8cb28d751770ad1d34b2348c98
CorentinFW <corentinfw@gmail.com>
2023-11-07 22:00:00 +0100

    algo 4 tp2 exo 1 a finir (une fonction dans le cours)

--- S5/Algo_4/TP2_Exhaustif/TP2-exo1.py
+++ S5/Algo_4/TP2_Exhaustif/TP2-exo1.py
@@ -33,46 +33,79 @@ def affiche(F):
     return s
 
 ################################################################
-
+'''
 def valide(F, A):
-    #
-    # A COMPLETER       
-    # 
-   return True
+    soluce = []
+    for i in range(len(F)):
+        for j in range(len(F)):
+            if A[j]>0:
+                soluce.append(1)
+        if len(A)<j:
+            soluce.append(0)
+            return False
+    return True
+'''
+def valide(F, A):
+    soluce = []
+    for i in range(len(F)):
+        soluce.append(0)
+    for i in range(len(F)):
+        for j in range(len(F[i])):
+            if A[j]>0:
+                soluce[j] = 1
+                break
+        if soluce[j] != 1:
+            return False
+    return True
 
 ################################################################
 
 def aff_suivante(A):
-    #
-    # A COMPLETER       
-    #i = 0
+    i = len(A)-1
+    while i >=0 and A[i] == 1:
+        A[i] = -1
+        i-=1
+    if i == -1:
+        return None
+    A[i] = 1
     return A
 
 def test_aff_suivante(n):
-    #
-    # A COMPLETER       
-    #
-    return None
+    A = []
+    for i in range(n):
+        A.append(-1)
+    for i in range(2**n):
+        print(aff_suivante(A))
         
 #########################################################################
 
 def sat_exhau(F, n):
     A = [-1] * n
-    #
-    # A COMPLETER       
-    #
+    for i in range(2**n):
+        if valide(F,A) == True:
+            return A
+        else:
+            aff_suivante(A)
     return A
 
 def elimination(F, n, b):
 #    "Formule psi = F(x_1, …, x_{n-1}, b)"
-    #
-    # A COMPLETER       
-    #
     psi=[]
+
+    for C in F:
+        Cp = []
+        sat = False
+        for l in C:
+            if len(l) == n and l*b >0:
+                sat = True
+            elif len(l) != n:
+                Cp.append[l]
+        if not(sat):
+            psi.append(Cp)
     return psi
 
 def sat_backtrack(F, n):
-    #
+    # est dans le cours
     # A COMPLETER       
     #
     return None
@@ -87,11 +120,12 @@ print("Récupérée sous forme de tableau: ",F)
 print("Nombre de variables: ",n)
 print("Formule SAT: ",affiche(F))
 print("-------------------------------------------------------")
-#print(valide(F, [1,1,-1]))
-#print(valide(F, [-1,-1,1]))
-#print("-------------------------------------------------------")
+print(valide(F, [1,1,-1]))
+print(valide(F, [-1,-1,1]))
+print("-------------------------------------------------------")
 print(test_aff_suivante(4))
 print("-------------------------------------------------------")
+print("taille de n = ",n)
 print(sat_exhau(F, n))
 print(sat_backtrack(F, n))
 
