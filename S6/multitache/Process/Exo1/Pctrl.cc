d37261b5d41211fe3703c80f08cfc39705f8ab86
Fay Corentin <corentinfw@gmail.com>
2024-04-22 17:47:28 +0200

    multitache file process

---
+++ S6/multitache/Process/Exo1/Pctrl.cc
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+
+typedef struct Msg {
+    long monetiquette;
+    long msg;
+} msg;
+
+int main(int argc, char * argv[]) {
+    key_t cle = ftok("./pourCle.txt", 10);
+
+    int f_id = msgget(cle, IPC_CREAT|0666);
+
+    for(int i = 0; i < 3; i++) {
+        printf("Pctrl : Je recupere une demande d'acces\n");
+
+        msg demande_acces;
+        int res = msgrcv(f_id, &demande_acces, (size_t) sizeof(demande_acces.msg), (long) 1, 0);
+
+        printf("Pctrl : J'ai une demande de la part de %ld\n", demande_acces.msg);
+        printf("Pctrl : Je donne l'acces a %ld\n", demande_acces.msg);
+
+        msg donne_acces = {.monetiquette = demande_acces.msg, .msg = 0};
+        res = msgsnd(f_id, &donne_acces, (size_t) sizeof(demande_acces.msg), 0);
+
+        printf("Pctrl : J'attend que %ld finisse\n", demande_acces.msg);
+
+        msg fin_access;
+        res = msgrcv(f_id, &fin_access, (size_t) sizeof(fin_access.msg), (long) 2, 0);
+    }
+
+    printf("Pctrl : Je termine\n");
+
+}
---
+++ S6/multitache/Process/Exo1/Pi.cc
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+typedef struct Msg {
+    long monetiquette;
+    long msg;
+} msg;
+
+int main(int argc, char * argv[]) {
+    key_t cle = ftok("./pourCle.txt", 10);
+
+    int f_id = msgget(cle, 0);
+
+    long monPid = getpid() + 10;
+
+    printf("Pi : Je demande l'acces a la ressource, pid = %ld\n", monPid);
+    printf("Pi : Je depose le message, pid = %ld\n", monPid);
+
+    msg demande_acces = {.monetiquette = 1, .msg = monPid};
+    int res = msgsnd(f_id, &demande_acces, (size_t) sizeof(demande_acces.msg), 0);
+
+    printf("Pi : Je recupere le message, pid = %ld\n", monPid);
+
+    msg donne_acces;
+    res = msgrcv(f_id, &donne_acces, (size_t) sizeof(donne_acces.msg), monPid, 0);
+
+    printf("Pi : Je commence a travailler, pid = %ld\n", monPid);
+
+    sleep(5);
+
+    printf("Pi : Je libere l'access, pid = %ld\n", monPid);
+
+    msg fin_access = {.monetiquette = 2, .msg = monPid};
+    res = msgsnd(f_id, &fin_access, (size_t) sizeof(fin_access.msg), 0);
+
+    printf("Pi : Je fini, pid = %ld\n", monPid);
+}
