51db1bf1c9a7ae0bef5995b01ee4d4bf66d211b5
CorentinFW <corentinfw@gmail.com>
2024-04-25 01:52:38 +0200

    verif correction tp4

--- S6/Verif/CoqTP4.v
+++ S6/Verif/CoqTP4.v
@@ -23,3 +23,33 @@ apply Is_Fact_S.
 apply IHn.
 reflexivity.
 Qed.
+
+Lemma my_fact_ind : forall P : nat -> nat -> Prop,
+    P 0 1 -> (forall n, P n (fact n) -> P (S n) (S n * fact n)) -> forall n, P n (fact n).
+Proof.
+induction n.
+assumption.
+apply H0.
+assumption.
+Qed.
+
+Print my_fact_ind.
+
+Require Import FunInd.
+Functional Scheme fact_ind := Induction for fact Sort Prop.
+Print fact_ind.
+
+Lemma correct_fact_fun_ind1 :
+forall n m, fact n = m -> IsFact n m.
+Proof.
+intro.
+functional induction (fact n) using fact_ind.
+intros x EQ.
+rewrite <- EQ.
+apply Is_Fact_0.
+intros x EQ.
+rewrite <- EQ.
+apply Is_Fact_S.
+apply IHn0.
+reflexivity.
+Qed.
