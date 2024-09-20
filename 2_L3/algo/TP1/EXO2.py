035e66eaf12caf939d6ce05a17e18cb1c60a2a7a
CorentinFW <corentinfw@gmail.com>
2024-09-20 13:09:19 +0200

    algo 4 tp 1 exo 2 fini

--- 2_L3/algo/TP1/EXO2.py
+++ 2_L3/algo/TP1/EXO2.py
@@ -81,31 +81,32 @@ def TabFin(n,a,b,k):
     return L
 
 #phase de test de temps
-n = randint(1000,10000)
-a = randint(1,10)
-b= randint(11,20)
-k= randint(n//2,n-1)
-#test 1 
-debut=time()
-eltMajDet(TabAlea(n,a,b,k))
-print(time()-debut)
-debut=time()
-eltMajProba(TabAlea(n,a,b,k))
-print(time()-debut)
-#test 2
-debut=time()
-eltMajDet(TabDeb(n,a,b,k))
-print(time()-debut)
-debut=time()
-eltMajProba(TabDeb(n,a,b,k))
-print(time()-debut)
-#test 3 
-debut=time()
-eltMajDet(TabFin(n,a,b,k))
-print(time()-debut)
-debut=time()
-eltMajProba(TabFin(n,a,b,k))
-print(time()-debut)
+
+# n = randint(1000,10000)
+# a = randint(1,10)
+# b= randint(11,20)
+# k= randint(n//2,n-1)
+# #test 1 
+# debut=time()
+# eltMajDet(TabAlea(n,a,b,k))
+# print(time()-debut)
+# debut=time()
+# eltMajProba(TabAlea(n,a,b,k))
+# print(time()-debut)
+# #test 2
+# debut=time()
+# eltMajDet(TabDeb(n,a,b,k))
+# print(time()-debut)
+# debut=time()
+# eltMajProba(TabDeb(n,a,b,k))
+# print(time()-debut)
+# #test 3 
+# debut=time()
+# eltMajDet(TabFin(n,a,b,k))
+# print(time()-debut)
+# debut=time()
+# eltMajProba(TabFin(n,a,b,k))
+# print(time()-debut)
 
 #Fin de la phase de test 
 
@@ -120,7 +121,33 @@ def contientEltMajMTirage(T, m):
         for j in T:
             if j == i:
                 cpt += 1
-        if cpt>= len(T):
-            return i
-print(contientEltMajMTirage([1,2,3,4,5,8,5,5,5,5,5,5,5,5,5],5))
+        if cpt>= (len(T)//2):
+            return True
+    return False
 
+
+def testContient(n,a,b,k,m,N):
+    reussite = 0
+    for i in range(N):
+        L = TabAlea(n,a,b,k)
+        if contientEltMajMTirage(L,m):
+            reussite += 1
+    return reussite
+a = 1
+b= 10
+TC = []
+# for k in range(10):
+#     TC.append(testContient(1000,a,b,(k*50)+500,1,1000))
+# plt.plot(TC)
+# plt.show()
+
+#plus nous avons d'itiration plus le taux de reussite augmente mais lentement 
+
+
+k = 500
+for m in range(10):
+    TC.append(testContient(1000,a,b,k,m,1000))
+plt.plot(TC)
+plt.show()
+
+#On peut en conclure que plus nous avons de tirage pour contientEltMajMTirage,m, plus vite le taux de reussite est grand et augment tres vite
