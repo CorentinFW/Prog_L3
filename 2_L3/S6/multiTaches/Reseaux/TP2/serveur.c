3c164af50aac087be4cabfc79781c4da7d0c1826
Fay Corentin <corentinfw@gmail.com>
2025-02-14 11:28:11 +0100

    sendtcp fini recv me ratio

--- 2_L3/S6/multiTaches/Reseaux/TP2/serveur.c
+++ 2_L3/S6/multiTaches/Reseaux/TP2/serveur.c
@@ -1,118 +1,121 @@
-#include <netinet/in.h>
-#include <stdio.h> 
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include<arpa/inet.h>
-#include<string.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 /* Programme serveur */
 /* 32768 et ip 162.38.84.75 */
-//char *mesfin = (char*)malloc(tailleTCP)
-//memcpy permet de copier le msg hasardeux par le vrai créer 
+// char *mesfin = (char*)malloc(tailleTCP)
+// memcpy permet de copier le msg hasardeux par le vrai créer
 
-int recvTCP(int sock,void * msg){
-
-   int sizeMsg;
-   int tailleREC = recv(sock, &sizeMsg,sizeof(sizeMsg) , 0);
-   if (tailleREC == -1){
-    perror("Client : pb pour recevoir le msg :");
-    exit(1); // je choisis ici d'arrêter le programme car le reste
-	     // dépendent de la réussite de la création de la socket.
+void *recvTCP(int sock, void *msg, int sizeMsg) {
+  int tailleR = 0;
+  while (tailleR < sizeMsg) {
+    int recu = recv(sock, &msg + tailleR, sizeMsg - tailleR, 0);
+    if (recu == -1) {
+      perror("Client : pb pour recevoir le msg :");
+      exit(1); // je choisis ici d'arrêter le programme car le reste
+               // dépendent de la réussite de la création de la socket.
+    }
+    tailleR = tailleR + recu;
   }
-
-   int tailleR = 0;
-   char * msgF =(char*)malloc(sizeMsg);
-   int tailleM = sizeMsg;
-
-   while (sizeMsg) {
-   int recu = recv(sock, &msgF + tailleR, tailleM - tailleR,0);
-   if (recu == -1){
-    perror("Client : pb pour envoyer le msg :");
-    exit(1); // je choisis ici d'arrêter le programme car le reste
-	     // dépendent de la réussite de la création de la socket.
-  }
-  memcpy(msg + strlen(msg),msgF,recu);
-  tailleR = tailleR + recu;
-   }
-   free(msgF);
-   return tailleR;
+  return msg;
 }
+
+void *recvTailleEtMessageTCP(int sock) {
+  int tailleM;
+  int recT = recv(sock, &tailleM, sizeof(tailleM), 0);
+  if (recT == -1) {
+    perror("Client : pb pour recevoir la taille du msg");
+    exit(1); // je choisis ici d'arrêter le programme car le reste
+             // dépendent de la réussite de la création de la socket.
+  }
+  printf("taille du futur message : %d \n", tailleM);
+  void *msg = (void *)malloc(tailleM);
+  recvTCP(sock, msg, tailleM);
+  return msg;
+}
+
 int main(int argc, char *argv[]) {
 
-  /* Je passe en paramètre le numéro de port qui sera donné à la socket créée plus loin.*/
+  /* Je passe en paramètre le numéro de port qui sera donné à la socket créée
+   * plus loin.*/
 
   /* Je teste le passage de parametres. Le nombre et la nature des
      paramètres sont à adapter en fonction des besoins. Sans ces
      paramètres, l'exécution doit être arrétée, autrement, elle
      aboutira à des erreurs.*/
-  if (argc != 2){
+  if (argc != 2) {
     printf("utilisation : %s port_serveur\n", argv[0]);
     exit(1);
   }
 
-  /* Etape 1 : créer une socket */   
+  /* Etape 1 : créer une socket */
   int ds = socket(PF_INET, SOCK_STREAM, 0);
 
   /* /!\ : Il est indispensable de tester les valeurs de retour de
      toutes les fonctions et agir en fonction des valeurs
      possibles. Voici un exemple */
-  if (ds == -1){
+  if (ds == -1) {
     perror("Serveur : pb creation socket :");
     exit(1); // je choisis ici d'arrêter le programme car le reste
-	     // dépendent de la réussite de la création de la socket.
+             // dépendent de la réussite de la création de la socket.
   }
-  
+
   /* J'ajoute des traces pour comprendre l'exécution et savoir
      localiser des éventuelles erreurs */
   printf("Serveur : creation de la socket réussie \n");
-  
+
   // Je peux tester l'exécution de cette étape avant de passer à la
   // suite. Faire de même pour la suite : n'attendez pas de tout faire
   // avant de tester.
-  
+
   /* Etape 2 : Nommer la socket du seveur */
- struct sockaddr_in ad;
- ad.sin_family = AF_INET;
- ad.sin_addr.s_addr = INADDR_ANY;
- ad.sin_port = htons(atoi(argv[1]));
- int sockS = bind(ds,(struct sockaddr*)&ad,sizeof(ad));
- if (sockS == -1){
+  struct sockaddr_in ad;
+  ad.sin_family = AF_INET;
+  ad.sin_addr.s_addr = INADDR_ANY;
+  ad.sin_port = htons(atoi(argv[1]));
+  int sockS = bind(ds, (struct sockaddr *)&ad, sizeof(ad));
+  if (sockS == -1) {
     perror("Serveur : pb creation de la socket serve :");
     exit(1); // je choisis ici d'arrêter le programme car le reste
-	     // dépendent de la réussite de la création de la socket.
+             // dépendent de la réussite de la création de la socket.
   }
- 
+
   /* Etape 4 : recevoir un message du client (voir sujet pour plus de détails)*/
-   listen(ds,7);
-   struct sockaddr_in aC;
-   socklen_t lg = sizeof(struct sockaddr_in);
-   int dSC = accept(ds, (struct sockaddr*)&aC , &lg);
-   
-   if (dSC == -1){
+  listen(ds, 7);
+  struct sockaddr_in aC;
+  socklen_t lg = sizeof(struct sockaddr_in);
+  int dSC = accept(ds, (struct sockaddr *)&aC, &lg);
+
+  if (dSC == -1) {
     perror("Serveur : vote RN :");
     exit(1); // je choisis ici d'arrêter le programme car le reste
-	     // dépendent de la réussite de la création de la socket.
+             // dépendent de la réussite de la création de la socket.
   }
-   /*
-   int tailleM;
-   recv(dSC, &tailleM,sizeof(tailleM) , 0);
-   printf("taille du futur message : %d",tailleM);
-   char msg[200];
-   recv(dSC, msg, sizeof(msg),0);
-   printf("reçu : %s ",msg);
+  /*
+  int tailleM;
+  recv(dSC, &tailleM,sizeof(tailleM) , 0);
+  printf("taille du futur message : %d",tailleM);
+  char msg[200];
+  recv(dSC, msg, sizeof(msg),0);
+  printf("reçu : %s ",msg);
 */
-   char msg[100];
-   recvTCP(ds, msg);
+  char msg[4];
+  void* msg2 = recvTCP(ds,msg,3);
+  printf("voici le msg du client : %s \n",msg);
   /* Etape 5 : envoyer un message au serveur (voir sujet pour plus de détails)*/
-  int r = strlen(msg)-1;
+  int r = strlen(msg) - 1;
   send(dSC, &r, sizeof(r), 0);
   /* Etape 6 : fermer la socket (lorsqu'elle n'est plus utilisée)*/
   close(ds);
   close(dSC);
-  
+
   printf("Serveur : je termine\n");
   return 0;
 }
